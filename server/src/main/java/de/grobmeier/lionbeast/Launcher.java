88e62298fa9089ed000c0a96f54f7f792240ba63
Christian Grobmeier <grobmeier@gmail.com>
2013-01-10 10:10:30 +0100

    main class

---
+++ server/src/main/java/de/grobmeier/lionbeast/Launcher.java
@@ -0,0 +1,18 @@
+package de.grobmeier.lionbeast;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Lionbeast HTTP Server
+ */
+public class Launcher {
+    private static final Logger logger = LoggerFactory.getLogger(Launcher.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting Lionbeast");
+
+
+        logger.info("Shutdown Lionbeast");
+    }
+}
