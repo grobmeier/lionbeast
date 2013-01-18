f3e8f506786ab486b5d10358ae75e03d6871d718
Christian Grobmeier <grobmeier@gmail.com>
2013-01-18 06:32:14 +0100

    comments, fixed reference

--- server/src/main/java/de/grobmeier/lionbeast/BufferUtils.java
+++ server/src/main/java/de/grobmeier/lionbeast/BufferUtils.java
@@ -11,11 +11,21 @@ public class BufferUtils {
 
     private final static Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
+    /**
+     * Decodes a byte buffer to a char buffer with this servers encoding
+     * @param buffer the buffer to decode
+     * @return a char buffer decoded with the selected charset
+     */
     public static CharBuffer decode(ByteBuffer buffer) {
         return UTF8_CHARSET.decode(buffer);
     }
 
+    /**
+     * Encodes a string value to a byte buffer with this servers encoding
+     * @param value the string to encode
+     * @return the ByteBuffer
+     */
     public static ByteBuffer encode(String value) {
-        return Charset.forName("UTF-8").encode(value.toString());
+        return UTF8_CHARSET.encode(value.toString());
     }
 }
